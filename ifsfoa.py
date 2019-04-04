6d20d219420ac03f8e1f4e2e212e328f5317be5c
Ynjxsjmh <ynjxsjmh@gmail.com>
2019-04-05 00:11:31 +0800

    Half complete _local_seeding: remain how to choose cols_to_use

--- ifsfoa.py
+++ ifsfoa.py
@@ -69,14 +69,16 @@ class Forest (object):
         self.best_tree = []
         self.id = 0
 
+        self.tree_groups = []
+
     def _initialize_forest(self):
         '''
         Invovled variables:
         tree: list
-                Age | x | x | x | x | x | x | fitness  | dimension reduction
+                Age | x | x | x | x | x | x | fitness    | dimension reduction | id | ansters
                 x is either 0 or 1
             index:
-                0   | 1 | 2 | . | . | . |dim| dimension+1
+                0   | 1 | 2 | . | . | . |dim| dimension+1|    dimension+2      | +3 |
 
         self.best_tree: list
         '''
@@ -120,28 +122,38 @@ class Forest (object):
         new_trees = []
         for tree in self.forest:
             if tree[0] == 0:       # Perform local seeding on trees with Age 0
+                cols_to_use =      # some best index remain unchanged
                 # Randomly choose LSC variables of the selected tree
-                selected_index = random.sample(
-                    range(1, self.dimension + 1), self.LSC)
-                temp_tree = tree[:]
-                for index in selected_index:
-                    # change from 0 to 1 or vice versa.
-                    temp_tree[index] = 1 - temp_tree[index]
-                    fitness = self._get_accuracy(
-                        temp_tree[1: self.dimension + 1])
-                    dimension_reduction = self._get_dimension_reduction_rate(
-                        temp_tree[1: self.dimension + 1])  # 计算每个 tree 的降维率
-                    temp_tree[self.dimension + 1] = fitness
-                    temp_tree[self.dimension + 2] = dimension_reduction
-                    if temp_tree[self.dimension +
-                                 1] > tree[self.dimension + 1]:
-                        new_trees.append(temp_tree)
+                if tree[self.dimension+1] >= 0.85 * self.best_tree[self.dimension+1]:  # good trees
+                    new_trees.extend(self.__plant_trees(tree, cols_to_use, 2 * self.LSC))
+                else:
+                    new_trees.extend(self.__plant_trees(tree, cols_to_use, self.LSC))
 
             # Increase the Age of all trees new generated ones in the local
             # seeding stage by 1
             tree[0] = tree[0] + 1
 
         self.forest.extend(new_trees)  # Merge new trees into forest
+
+    def __plant_trees(self, father_tree, cols_to_use, LSC):
+        new_trees = []
+        selected_index = random.sample(cols_to_use, LSC)
+        for index in selected_index:
+            temp_tree = father_tree[:]
+            # change from 0 to 1 or vice versa.
+            temp_tree[index] = 1 - temp_tree[index]
+            fitness = self._get_accuracy(
+                temp_tree[1: self.dimension + 1])
+            dimension_reduction = self._get_dimension_reduction_rate(
+                temp_tree[1: self.dimension + 1])  # 计算每个 tree 的降维率
+            temp_tree[self.dimension + 1] = fitness
+            temp_tree[self.dimension + 2] = dimension_reduction
+            temp_tree[self.dimension + 3] = self.id
+            temp_tree.append(father_tree[self.dimension + 3])
+            self.id += 1
+            new_trees.append(temp_tree)
+
+        return new_trees
 
     def _population_limiting(self):
         for tree in self.forest:       # Trees with “Age” bigger than “life time” parameter
