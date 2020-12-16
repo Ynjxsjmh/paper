22cbac3d029117c76e8e4a4a6554bfefe790234e
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-12-17 01:47:24 +0800

    Separate header and implementation files

---
+++ Computing minimal hitting sets whith CHS-tree method/src/chs.h
@@ -0,0 +1,31 @@
+#include <string>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+
+class Node {
+    string edge;
+    vector<set<string> > set_cluster_before_simplified;
+    vector<set<string> > set_cluster_after_simplified;
+    set<string> extension_set;
+    vector<Node*> children;
+};
+
+
+class CHSTree {
+public:
+    CHSTree(vector<set<string> > set_cluster);
+    ~CHSTree();
+
+private:
+    void generateTree(Node* root);
+    vector<set<string> > simplifySetCuster(vector<set<string> > set_cluster);
+    vector<set<string> > getMinPotential(vector<set<string> > set_cluster);
+    set<string> getExtensionSet(vector<set<string> > set_cluster);
+    vector<set<string> > eraseElementsFromSetCuster(set<string> elements, vector<set<string> > set_cluster);
+    Node *root;
+};
